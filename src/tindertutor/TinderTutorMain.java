d91e255ed81518171944cbab5ab4b7d1eb21cd58
adamoc <oceallaighadam96@gmail.com>
2019-02-25 17:12:48 +0000

    Styled the Main Swipe Page and the Launch Page

--- src/tindertutor/TinderTutorMain.java
+++ src/tindertutor/TinderTutorMain.java
@@ -6,18 +6,21 @@
 package tindertutor;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -36,6 +39,7 @@ public class TinderTutorMain extends Application {
     
     Stage window;
     Scene scene2;
+    Scene scene1;
     
     @Override
     public void start(Stage primaryStage) {
@@ -43,13 +47,21 @@ public class TinderTutorMain extends Application {
         //Setting up the primaryStage to be switchable
         window = primaryStage;
         
+        //Restricting the posibility of making screen bigger
+        window.setResizable(false);
+        
         //Declaring all our objects that need to go on scene
         ImageView image = new ImageView(new Image(getClass().getResourceAsStream("Assets/TinderTutorLogo.png"),220,220,true,true));
-        Button btnLogin = new Button("Login");
-        Button btnSignUp = new Button("Sign Up");
-        Button btnLinkedIN = new Button("Linked In");
-        Button btnFacebook = new Button("Facebook");
-        Button btnGooglePlus = new Button("Google +");
+        Button btnLogin = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnLogin.png"),150,280,true,true)));
+        Button btnSignUp = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnSignUp.png"),150,280,true,true)));
+        btnLogin.setStyle("-fx-background-color: transparent");
+        btnSignUp.setStyle("-fx-background-color: transparent");
+        Button btnLinkedIn = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnLinkedIn.png"),150,280,true,true)));
+        Button btnFacebook = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnFacebook.png"),150,280,true,true)));
+        Button btnGooglePlus = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnGooglePlus.png"),150,280,true,true)));
+        btnLinkedIn.setStyle("-fx-background-color: transparent");
+        btnFacebook.setStyle("-fx-background-color: transparent");
+        btnGooglePlus.setStyle("-fx-background-color: transparent");
         
         //Adding all Button functionality in lambda expressions
         btnLogin.setOnAction(e -> {
@@ -61,12 +73,43 @@ public class TinderTutorMain extends Application {
             txtUsername.setMaxWidth(340);
             txtPassword.setMaxWidth(340);
             Label forgotPass = new Label("Forgot Your password ?");
-            btnLogin.setStyle("-fx-background-radius: 20px; -fx-padding: 4 65; -fx-background-color: #ef5350; -fx-text-fill: white; -fx-font-weight: bold");
-            tinderTutorLogin.LoginView(image, lblSignIn, txtUsername, txtPassword, forgotPass, btnLogin).setMargin(lblSignIn , new Insets(-40 ,0 ,0,0));
-            scene2 = new Scene(tinderTutorLogin.LoginView(image, lblSignIn, txtUsername, txtPassword, forgotPass, btnLogin) , 600 ,700);
+            Button btnLoginReal = new Button("Login");
+            btnLoginReal.setStyle("-fx-background-radius: 20px; -fx-padding: 4 65; -fx-background-color: #ef5350; -fx-text-fill: white; -fx-font-weight: bold");
+            tinderTutorLogin.LoginView(image, lblSignIn, txtUsername, txtPassword, forgotPass, btnLoginReal).setMargin(lblSignIn , new Insets(-40 ,0 ,0,0));
+            scene2 = new Scene(tinderTutorLogin.LoginView(image, lblSignIn, txtUsername, txtPassword, forgotPass, btnLoginReal) , 600 ,700);
             window.setScene(scene2);
-        });
         
+            btnLoginReal.setOnAction(event -> {
+                    if(txtUsername.getText().equals("Adam")&& txtPassword.getText().equals("1234")){
+
+                        TinderTutorMainSwipe tinderTutorSwipe = new TinderTutorMainSwipe();
+                        Button btnSettings = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnSettings.png"),80,80,true,true)));
+                        btnSettings.setStyle("-fx-background-color: transparent;");
+                        ImageView logoView = new ImageView(new Image(getClass().getResourceAsStream("Assets/TinderTutorLogo.png"),280,120,true,true));
+                        Button btnChat= new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnChat.png"),80,80,true,true)));
+                        btnChat.setStyle("-fx-background-color: transparent;");
+                        ImageView mainPicView = new ImageView(new Image(getClass().getResourceAsStream("Assets/MainTestProfilePic.jpg"),280,280,true,true));
+                        mainPicView.setStyle("-fx-background-radius: 50; -fx-background-color: red;");
+                        Label lblUsers_Name= new Label("Username : ");
+                        Label lblUsers_Qualification= new Label("Qualification: ");
+                        Label lblUsers_Subject= new Label("Subject: ");
+                        Label lblUsers_Location= new Label("Location: ");
+                        Label lblUsers_Price= new Label("Price: ");
+                        ImageView FormattingView = new ImageView(new Image(getClass().getResourceAsStream("Assets/FormattingMain.png"),400,200,false,true));
+                        Button btnCheckYes= new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnYes.png"),60,50,true,true)));
+                        Button btnCheckNo = new Button("", new ImageView(new Image(getClass().getResourceAsStream("Assets/btnNo.png"),60,50,true,true)));
+                        btnCheckYes.setStyle("-fx-background-color: transparent;");
+                        btnCheckNo.setStyle("-fx-background-color: transparent;");
+                        scene1 = new Scene(tinderTutorSwipe.SwipeView(btnSettings, logoView, btnChat, mainPicView, lblUsers_Name, lblUsers_Qualification, lblUsers_Subject, lblUsers_Location, lblUsers_Price, FormattingView , btnCheckYes, btnCheckNo) , 600 , 700);
+                        window.setScene(scene1);
+
+                    } else {
+                        Alert helpAlert = new Alert(Alert.AlertType.WARNING,  "Please enter correct login details");
+                        helpAlert.setTitle("Error");
+                        helpAlert.showAndWait();
+                    }
+                });
+            });
         // Setting the images position
         HBox imagePos = new HBox(image);
         imagePos.setAlignment(Pos.CENTER);
@@ -80,7 +123,7 @@ public class TinderTutorMain extends Application {
         buttonsPos.setMargin(btnSignUp, new Insets(-30,0,30,0));
         
         //Setting the social buttons positions
-        VBox socialButtons = new VBox(20, btnLinkedIN , btnFacebook , btnGooglePlus);
+        VBox socialButtons = new VBox(20, btnLinkedIn ,btnGooglePlus, btnFacebook );
         socialButtons.setAlignment(Pos.CENTER);
         
         //Setting up the VBox to take everything put it on itself and then add to the scene which is added to the stage 
@@ -98,10 +141,6 @@ public class TinderTutorMain extends Application {
 
 
     }
-
-    /**
-     * @param args the command line arguments
-     */
     
     
 }
--- src/tindertutor/TinderTutorMainSwipe.java
+++ src/tindertutor/TinderTutorMainSwipe.java
@@ -5,10 +5,13 @@
  */
 package tindertutor;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 /**
@@ -17,13 +20,29 @@ import javafx.scene.layout.VBox;
  */
 public class TinderTutorMainSwipe{
     
-    public VBox SwipeView(Button btnSettings , ImageView logoView , Button btnChat , ImageView mainPicView , Label lblUsers_Name , Label lblUsers_Qualification , Label lblUsers_Subject , Label lblUsers_Location , Label lblUsers_Price , Button btnCheckYes , Button btnCheckNo ){
+    public VBox SwipeView(Button btnSettings , ImageView logoView , Button btnChat , ImageView mainPicView , Label lblUsers_Name , Label lblUsers_Qualification , Label lblUsers_Subject , Label lblUsers_Location , Label lblUsers_Price , ImageView FormattingView,  Button btnCheckYes , Button btnCheckNo){
         HBox topSwipeView = new HBox(btnSettings , logoView , btnChat);
-        VBox middleSwipeView = new VBox(mainPicView ,lblUsers_Name, lblUsers_Qualification , lblUsers_Subject , lblUsers_Location , lblUsers_Price );
-        HBox bottomSwipeView = new HBox(btnCheckYes , btnCheckYes);
+        topSwipeView.setMargin(btnSettings, new Insets(10,80,0,0));
+        topSwipeView.setMargin(btnChat, new Insets(12,0,0,80));
+        topSwipeView.setAlignment(Pos.TOP_CENTER);
         
-        VBox overallLayout = new VBox(20);
-        overallLayout.getChildren().addAll(topSwipeView,middleSwipeView ,bottomSwipeView);
+        VBox middleSwipeView = new VBox(mainPicView ,lblUsers_Name, lblUsers_Qualification , lblUsers_Subject , lblUsers_Location , lblUsers_Price);
+        middleSwipeView.setAlignment(Pos.BOTTOM_CENTER);
+        middleSwipeView.setMargin(mainPicView, new Insets(0,0,20,0));
+        
+        HBox bottomSwipeView = new HBox(btnCheckNo, btnCheckYes);
+        bottomSwipeView.setAlignment(Pos.TOP_CENTER);
+        bottomSwipeView.setMargin(btnCheckNo, new Insets(0,80,0,0));
+        bottomSwipeView.setMargin(btnCheckYes, new Insets(0,0,0,80));
+        btnCheckYes.relocate(50, 100);
+        
+        VBox formatView = new VBox(FormattingView);
+        formatView.setMargin(FormattingView, new Insets(0,0,0,0));
+        formatView.setAlignment(Pos.BOTTOM_CENTER);
+        
+        VBox overallLayout = new VBox(20, topSwipeView , middleSwipeView , formatView, bottomSwipeView);
+        
+        
      return overallLayout;   
         
     }
