a90b1bf28e5b4c66ba8544f38b02faf0c7de2c46
adamoc <oceallaighadam96@gmail.com>
2019-03-07 15:41:06 +0000

    added Student Profiles

--- src/tindertutor/TinderTutorMain.java
+++ src/tindertutor/TinderTutorMain.java
@@ -6,19 +6,23 @@
 package tindertutor;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -41,6 +45,7 @@ public class TinderTutorMain extends Application {
     Scene SwipePage;
     Scene LoginRealPage;
     Scene SignUpPage;
+    Scene SignUpStudentPage;
     Scene SettingsPage;
     
     @Override
@@ -81,13 +86,79 @@ public class TinderTutorMain extends Application {
             Label lblTeacher = new Label("Teacher ?");
             
             btnStudent.setOnAction(event-> {
-                System.out.println("Well");
+                TinderTutorSignUpStudent tindertutorSignUpStudent = new TinderTutorSignUpStudent();
+                
+                ImageView logoView2 = new ImageView(new Image(getClass().getResourceAsStream("Assets/TinderTutorLogo.png"),120,120,true,true));
+                
+                Label lblStudentProf = new Label("Student Profile");
+                lblStudentProf.setTextFill(Color.RED);
+                lblStudentProf.setStyle("-fx-font-size: 20;");
+                
+                Image mainImage = new Image(getClass().getResourceAsStream("Assets/MainTestProfilePic.jpg"),280,280,true,true);
+                Circle mainPicView = new Circle(250,250,50);
+                mainPicView.setFill(new ImagePattern(mainImage));
+                
+                Label lblUsers_Name = new Label("Name");
+                TextField txtUsers_Name = new TextField();
+                
+                Label lblUsers_Email = new Label("Email");
+                TextField txtUsers_Email = new TextField();
+                
+                Label lblUsers_Pass = new Label("Password");
+                PasswordField passUsers_Email = new PasswordField(); 
+                
+                ImageView loginDetails = new ImageView(new Image(getClass().getResourceAsStream("Assets/LoginDetailsLine.png"),400,400,true,true));
+                
+                Label lblUsers_Year = new Label("Year");
+                TextField txtUsers_Year = new TextField();
+                
+                Label lblUsers_Course = new Label("Course");
+                TextField txtUsers_Course = new TextField();
+                
+                Label lblUsers_Module = new Label("Module");
+                TextField txtUsers_Module = new TextField();
+                
+                Label lblLocationSlider = new Label("Location");
+                
+                Slider locationSlider = new Slider(0, 1, 0.5);
+                locationSlider.setShowTickMarks(true);
+                locationSlider.setShowTickLabels(true);
+                locationSlider.setMajorTickUnit(0.25f);
+                locationSlider.setBlockIncrement(0.1f);
+                
+                Label lblUsers_preferredPrice = new Label("Price");
+                ObservableList<String> preferredPriceOptions = 
+                FXCollections.observableArrayList(
+                    "1.00",
+                    "2.00",
+                    "3.00"
+                );
+                ComboBox priceComboBox = new ComboBox(preferredPriceOptions);
+                
+                Label lblUsers_Type = new Label("Type");
+                
+                ToggleGroup typeGroup = new ToggleGroup();
+                RadioButton button1 = new RadioButton("Single");
+                button1.setToggleGroup(typeGroup);
+                button1.setSelected(true);
+                RadioButton button2 = new RadioButton("Group");
+                button2.setToggleGroup(typeGroup);
+                
+                
+                Button btnSignUpReal = new Button("Sign Up");
+
+                SignUpStudentPage = new Scene(tindertutorSignUpStudent.SignUpStudentView(logoView2,lblStudentProf ,mainPicView, lblUsers_Name,txtUsers_Name,lblUsers_Email,txtUsers_Email,lblUsers_Pass,passUsers_Email, loginDetails,lblUsers_Year, txtUsers_Year,lblUsers_Course, txtUsers_Course,lblUsers_Module, txtUsers_Module, lblLocationSlider, locationSlider, lblUsers_preferredPrice , priceComboBox , lblUsers_Type , button1 , button2,  btnSignUpReal), 600 ,700);
+              
+                window.setScene(SignUpStudentPage);
             });
+            
+            
             btnTeacher.setOnAction(evnt -> {
                 System.out.println("Boi");
             });
             
-            SignUpPage = new Scene(tindertutorSignUp.SignUpView(logoView,lblSignUp , lblUserType, btnStudent, btnTeacher,  lblStudent , lblTeacher), 600 ,700);
+            SignUpPage = new Scene(tindertutorSignUp.SignUpView(logoView,lblSignUp , lblUserType, btnStudent, btnTeacher,  lblStudent , lblTeacher ), 600 ,700);
+              
             window.setScene(SignUpPage);
         });
         
@@ -106,7 +177,6 @@ public class TinderTutorMain extends Application {
             tinderTutorLogin.LoginView(image, lblSignIn, txtUsername, txtPassword, forgotPass, btnLoginReal).setMargin(lblSignIn , new Insets(-40 ,0 ,0,0));
             LoginRealPage = new Scene(tinderTutorLogin.LoginView(image, lblSignIn, txtUsername, txtPassword, forgotPass, btnLoginReal) , 600 ,700);
             window.setScene(LoginRealPage);
-        
             btnLoginReal.setOnAction(event -> {
                     if(txtUsername.getText().equals("Adam")&& txtPassword.getText().equals("1234")){
 
@@ -145,6 +215,75 @@ public class TinderTutorMain extends Application {
                             Label lblUserType = new Label("Student");
                             Label lblUniName = new Label("Stanford");
                             Button btnUserProfile = new Button("My Profile");
+                            
+                            btnUserProfile.setOnAction(evet -> {
+                              TinderTutorSignUpStudent tindertutorSignUpStudent = new TinderTutorSignUpStudent();
+                
+                                ImageView logoView2 = new ImageView(new Image(getClass().getResourceAsStream("Assets/TinderTutorLogo.png"),120,120,true,true));
+
+                                Label lblStudentProf = new Label("Student Profile");
+                                lblStudentProf.setTextFill(Color.RED);
+                                lblStudentProf.setStyle("-fx-font-size: 20;");
+
+                                Image mainImage2 = new Image(getClass().getResourceAsStream("Assets/MainTestProfilePic.jpg"),280,280,true,true);
+                                Circle mainPicView2 = new Circle(250,250,50);
+                                mainPicView2.setFill(new ImagePattern(mainImage2));
+
+                                Label lblUsers_Name2 = new Label("Name");
+                                TextField txtUsers_Name = new TextField();
+
+                                Label lblUsers_Email = new Label("Email");
+                                TextField txtUsers_Email = new TextField();
+
+                                Label lblUsers_Pass = new Label("Password");
+                                PasswordField passUsers_Email = new PasswordField(); 
+
+                                ImageView loginDetails = new ImageView(new Image(getClass().getResourceAsStream("Assets/LoginDetailsLine.png"),400,400,true,true));
+
+                                Label lblUsers_Year = new Label("Year");
+                                TextField txtUsers_Year = new TextField();
+
+                                Label lblUsers_Course = new Label("Course");
+                                TextField txtUsers_Course = new TextField();
+
+                                Label lblUsers_Module = new Label("Module");
+                                TextField txtUsers_Module = new TextField();
+
+                                Label lblLocationSlider = new Label("Location");
+
+                                Slider locationSlider = new Slider(0, 1, 0.5);
+                                locationSlider.setShowTickMarks(true);
+                                locationSlider.setShowTickLabels(true);
+                                locationSlider.setMajorTickUnit(0.25f);
+                                locationSlider.setBlockIncrement(0.1f);
+
+                                Label lblUsers_preferredPrice = new Label("Price");
+                                ObservableList<String> preferredPriceOptions = 
+                                FXCollections.observableArrayList(
+                                    "1.00",
+                                    "2.00",
+                                    "3.00"
+                                );
+                                ComboBox priceComboBox = new ComboBox(preferredPriceOptions);
+
+                                Label lblUsers_Type = new Label("Type");
+
+                                ToggleGroup typeGroup = new ToggleGroup();
+                                RadioButton button1 = new RadioButton("Single");
+                                button1.setToggleGroup(typeGroup);
+                                button1.setSelected(true);
+                                RadioButton button2 = new RadioButton("Group");
+                                button2.setToggleGroup(typeGroup);
+
+
+                                Button btnSignUpReal = new Button("Sign Up");
+
+                                SignUpStudentPage = new Scene(tindertutorSignUpStudent.SignUpStudentView(logoView2,lblStudentProf ,mainPicView2, lblUsers_Name2,txtUsers_Name,lblUsers_Email,txtUsers_Email,lblUsers_Pass,passUsers_Email, loginDetails,lblUsers_Year, txtUsers_Year,lblUsers_Course, txtUsers_Course,lblUsers_Module, txtUsers_Module, lblLocationSlider, locationSlider, lblUsers_preferredPrice , priceComboBox , lblUsers_Type , button1 , button2,  btnSignUpReal), 600 ,700);
+
+                                window.setScene(SignUpStudentPage);
+                            });
+                            
+                            
                             btnUserProfile.setStyle("-fx-background-radius: 20px; -fx-padding: 4 65; -fx-background-color: #ef5350; -fx-text-fill: white; -fx-font-weight: bold");
                             ImageView logoPlusView = new ImageView(new Image(getClass().getResourceAsStream("Assets/TinderTutorLogoPlus.png"),150,100,false,true));
                             SettingsPage = new Scene(tinderTutorSettings.SettingsView(settingsView, formatView ,userPicView , lblUserName ,lblUserType ,lblUniName, btnUserProfile ,logoPlusView), 600, 700);
@@ -174,12 +313,14 @@ public class TinderTutorMain extends Application {
         VBox socialButtons = new VBox(20, btnLinkedIn ,btnGooglePlus, btnFacebook );
         socialButtons.setAlignment(Pos.CENTER);
         
+        
         //Setting up the VBox to take everything put it on itself and then add to the scene which is added to the stage 
         VBox root = new VBox(imagePos , buttonsPos , socialButtons);
         root.setAlignment(Pos.CENTER);
         
         //Setting up the scene
         Scene scene = new Scene(root , 600 , 700);
+         
         
         //Setting up the stage
         primaryStage.setTitle("Tutor Tinder");
--- src/tindertutor/TinderTutorSignUpStudent.java
+++ src/tindertutor/TinderTutorSignUpStudent.java
@@ -7,34 +7,86 @@ package tindertutor;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 
 /**
  *
  * @author adamoc
  */
 public class TinderTutorSignUpStudent {
-     public VBox SignUpStudentView( ImageView logoView, Label lblSignUp , Label lbluserType , Button btnStudent , Button btnTeacher , Label lblStudent , Label lblTeacher){
+     public VBox SignUpStudentView( ImageView logoView2 , Label lblStudentProf , Circle mainPicView , Label lblUsers_Name , TextField txtUsers_Name , Label lblUsers_Email , TextField txtUsers_Email , Label lblUsers_Pass , PasswordField txtUsers_Pass , ImageView loginDetails , Label lblUsers_Year ,  TextField txtUsers_Year , Label lblUsers_Course , TextField txtUsers_Course , Label lblUsers_Module , TextField txtUsers_Module ,Label lblLocationSlider, Slider locationSlider , Label lblUsers_PreferredPrice , ComboBox priceComboBox , Label lblUsers_Type , RadioButton button1 , RadioButton button2 ,  Button btnSignUpReal ){
         
-        VBox topView = new VBox( logoView,  lblSignUp, lbluserType); 
-        topView.setAlignment(Pos.TOP_CENTER);
-        topView.setMargin(lbluserType, new Insets(70,0,0,0));
+        HBox topLogoView = new HBox(logoView2); 
+        topLogoView.setAlignment(Pos.TOP_RIGHT);
+
+        HBox topViewLabel = new HBox(lblStudentProf);
+        topViewLabel.setAlignment(Pos.TOP_CENTER);
+        topViewLabel.setMargin(lblStudentProf ,new Insets(-40,0,0,0));
         
-        HBox middleView = new HBox(btnStudent , btnTeacher);
-        middleView.setAlignment(Pos.CENTER);
-        middleView.setMargin(btnStudent, new Insets(10,0,0,0));
-        middleView.setMargin(btnTeacher, new Insets(10,0,0,0));
+        HBox topUserImageView = new HBox(mainPicView , lblUsers_Name , txtUsers_Name);
+        topUserImageView.setAlignment(Pos.CENTER_LEFT);
+        topUserImageView.setMargin(mainPicView,new Insets(0,20,0,30));
+        topUserImageView.setMargin(lblUsers_Name, new Insets(-20,10,0,40));
+        topUserImageView.setMargin(txtUsers_Name, new Insets(-20,10,0,5));
+        
+        HBox topFirstView = new HBox(lblUsers_Email ,txtUsers_Email);
+        topFirstView.setMargin(lblUsers_Email, new Insets(-65,12,0,10));
+        topFirstView.setMargin(txtUsers_Email, new Insets(-65,12,0,5));
+        topFirstView.setAlignment(Pos.CENTER);
+        
+        HBox topSecondView = new HBox(lblUsers_Pass ,txtUsers_Pass);
+        topSecondView.setMargin(lblUsers_Pass, new Insets(-20,18,0,-10));
+        topSecondView.setMargin(txtUsers_Pass, new Insets(-20,20,0,0));
+        topSecondView.setAlignment(Pos.CENTER);
         
         
-        HBox bottomView = new HBox(lblStudent , lblTeacher);
-        bottomView.setAlignment(Pos.BOTTOM_CENTER);
-        bottomView.setSpacing(150);
+        HBox topThirdView = new HBox(loginDetails);
+        topThirdView.setAlignment(Pos.CENTER);
         
-        VBox layout = new VBox(20 ,topView , middleView , bottomView);
+        HBox middleFirstView = new HBox(lblUsers_Year,txtUsers_Year );
+        topSecondView.setMargin(lblUsers_Year, new Insets(0,18,0,10));
+        topSecondView.setMargin(txtUsers_Year, new Insets(0,20,0,5));
+        middleFirstView.setAlignment(Pos.CENTER);
+        
+        HBox middleSecondView = new HBox(lblUsers_Course , txtUsers_Course);
+        middleSecondView.setMargin(lblUsers_Course, new Insets(0,18,0,-10));
+        middleSecondView.setMargin(txtUsers_Course, new Insets(0,20,0,10));
+        middleSecondView.setAlignment(Pos.CENTER);
+        
+        HBox middleThirdView = new HBox(lblUsers_Module , txtUsers_Module );
+        middleThirdView.setMargin(lblUsers_Module, new Insets(0,18,0,-20));
+        middleThirdView.setAlignment(Pos.CENTER);
+        
+        HBox middleFourthView = new HBox(lblLocationSlider , locationSlider );
+        middleFourthView.setMargin(lblLocationSlider , new Insets(0,18,0,-80));
+        middleFourthView.setAlignment(Pos.CENTER);
+        
+        HBox middleFifthView = new HBox(lblUsers_PreferredPrice, priceComboBox );
+        middleFifthView.setMargin(lblUsers_PreferredPrice, new Insets(0,18,0,-20));
+        middleFifthView.setAlignment(Pos.CENTER);
+        
+        HBox middleSixthView = new HBox(lblUsers_Type , button1 , button2);
+        
+        middleSixthView.setAlignment(Pos.CENTER);
+        
+        HBox middleLastView = new HBox(btnSignUpReal);
+        middleLastView.setAlignment(Pos.CENTER);
+        
+        
+        
+        VBox layout = new VBox(20 ,topLogoView , topViewLabel ,topUserImageView,topFirstView, topSecondView , topThirdView , middleFirstView ,middleSecondView, middleThirdView, middleFourthView, middleFifthView , middleSixthView , middleLastView );
         
         
         
